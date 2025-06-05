878e06a957219bdb0da8eaabbacce9504c0d7cb3
edgar <edgar.andre@ynov.com>
2025-06-05 10:22:35 +0200

    feat: front of login register

---
+++ front-app/src/routes/Login.svelte
@@ -0,0 +1,14 @@
+<script>
+  let email = '';
+  let password = '';
+  const login = () => {
+    // handle login logic
+    console.log({ email, password });
+  };
+</script>
+
+<form on:submit|preventDefault={login}>
+  <input type="email" bind:value={email} placeholder="Email" />
+  <input type="password" bind:value={password} placeholder="Password" />
+  <button type="submit">Login</button>
+</form>
---
+++ front-app/src/routes/Register.svelte
@@ -0,0 +1,14 @@
+<script>
+  let email = '';
+  let password = '';
+  const register = () => {
+    // handle register logic
+    console.log({ email, password });
+  };
+</script>
+
+<form on:submit|preventDefault={register}>
+  <input type="email" bind:value={email} placeholder="Email" />
+  <input type="password" bind:value={password} placeholder="Password" />
+  <button type="submit">Register</button>
+</form>
